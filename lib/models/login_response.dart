ed6a817226d1c79dc31e318058a551941db59a99
Stn77 <fudinyusuf20@gmail.com>
2026-01-14 08:40:11 +0700

    Remove unused ProfileResponse and ProfileData classes from login_response.dart

--- lib/models/login_response.dart
+++ lib/models/login_response.dart
@@ -1,5 +1,4 @@
 import 'user_model.dart';
-import 'siswa_model.dart';
 
 // ===== models/login_response.dart =====
 class LoginResponse {
@@ -26,55 +25,6 @@ class LoginResponse {
       'message': message,
       'token': token,
       'user': user.toJson(),
-    };
-  }
-}
-
-// ===== models/profile_response.dart =====
-class ProfileResponse {
-  final String status;
-  final ProfileData data;
-
-  ProfileResponse({
-    required this.status,
-    required this.data,
-  });
-
-  factory ProfileResponse.fromJson(Map<String, dynamic> json) {
-    return ProfileResponse(
-      status: json['status'],
-      data: ProfileData.fromJson(json['data']),
-    );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'status': status,
-      'data': data.toJson(),
-    };
-  }
-}
-
-class ProfileData {
-  final UserModel user;
-  final SiswaModel siswa;
-
-  ProfileData({
-    required this.user,
-    required this.siswa,
-  });
-
-  factory ProfileData.fromJson(Map<String, dynamic> json) {
-    return ProfileData(
-      user: UserModel.fromJson(json['user']),
-      siswa: SiswaModel.fromJson(json['siswa']),
-    );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'user': user.toJson(),
-      'siswa': siswa.toJson(),
     };
   }
 }
